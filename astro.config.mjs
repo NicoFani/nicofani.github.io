0d2d45e6ca1fc44d61d328b86f42833022b1cb1b
NicoFani <nicolasfani87@gmail.com>
2025-02-15 17:02:29 -0300

    fix problem of deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from '@astrojs/tailwind'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nicofani.github.io',
-  base: 'nicofani.github.io',
   integrations: [tailwind()]
 })
